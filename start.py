8327d31d9b504b8ce312c42f0a3d356b14ff8a9a
RedWalof <166923509+RedWalof@users.noreply.github.com>
2025-02-15 20:11:09 +0100

    Update start.py

--- start.py
+++ start.py
@@ -45,7 +45,7 @@ async def notify_support_channel(user, channel_id):
     
     try:
         # Envoyer un message dans le salon
-        await channel.send(f"⚠️ {user.mention} attend dans le salon **Attente Support**  @￣￣ STAFF ￣￣ .")
+        await channel.send(f"⚠️ {user.name} attend dans le salon **Attente Support**  @Equipe Staff .")
         print(f"Message envoyé dans le salon {channel.name} ({channel.id}) pour {user.name}.")
     except Exception as e:
         print(f"Erreur lors de l'envoi du message dans le salon {channel_id}: {e}")
